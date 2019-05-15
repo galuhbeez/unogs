ef49baeff2f305a6c1452e26338b266814c46c3d
galuhbeez <galuhbeez@gmail.com>
2019-05-14 21:55:10 -0700

    cleaned up vars and comments

--- netczech.py
+++ netczech.py
@@ -6,6 +6,8 @@ import requests
 
 imdb = Imdb()
 
+
+#get the movie title to search and limit results 6>
 movie_title = raw_input("Enter movie name:")
 
 s_result = imdb.search_for_title(movie_title)
@@ -22,27 +24,20 @@ if max_values > 5:
 print "here are your results"
 
 for i in xrange(0,max_values,1):
-        output="{index} - {title} {year} ".format(index=i+1,title=s_result[i]['title'], year=s_result[i]['year']) 
-	#print s_result[i]
+        output="{index} - {title} {year} ".format(index=i+1,title=s_result[i]['title'], year=s_result[i]['year'])
 	print output
 
 selection = raw_input("Please enter selection number:")
 
-
-dknight = s_result[int(selection)-1]
-
+t_search = s_result[int(selection)-1]
 
 
-#print(type(dknight))
-
-#print dknight
-
-#print(dknight['imdb_id'])
-dbid = (dknight['imdb_id'])
+#use the imdb_id search result to get the list of countries title is available
+dbid = (t_search['imdb_id'])
 
 response = requests.get(("https://unogs-unogs-v1.p.mashape.com/aaapi.cgi?t=loadvideo&q=" + dbid),
   headers={
-    "X-Mashape-Key": "<your key here>",
+    "X-Mashape-Key": "",
     "Accept": "application/json"
   }
 )
@@ -50,10 +45,8 @@ response = requests.get(("https://unogs-unogs-v1.p.mashape.com/aaapi.cgi?t=loadv
 if len(response.text) == 0:
 	print "fuck off"
 	exit(1)
-#print 'bigdick mofo: {} <<'.format(response.text)
 
 json_data = json.loads(response.text)
-
 
 country_list = json_data['RESULT']['country']
 
@@ -63,13 +56,8 @@ for c in country_list:
 
 print countries
 
+#check if available in the us
 if 'us' in countries:
 	print "USA OK!"
 else:
 	print "Not available in the U.S."
-
-#        if 'us' in c['ccode']:
-#                print "USA OK!"
-#        else:
-#                print "Better Luck Next Time"
-
